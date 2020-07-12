6f735a41522059040692f546b41d0f577b0cb85e
branwn <50729260+branwn@users.noreply.github.com>
2020-07-12 20:22:30 +0800

    update

--- springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/controller/admin/UserController.java
+++ springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/controller/admin/UserController.java
@@ -6,24 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
 
-    @ResponseBody
-    @RequestMapping("/helloworld")
-    public String hello() {
-        return "Hello World";
-    }
+
 
     @RequestMapping("/userList")
     public String userList2(ModelMap map) {
@@ -31,6 +29,11 @@ public class UserController {
         List<User> List = userService.listAll();
         map.addAttribute("List", List);
         return "admin/userList";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public User findByName(@RequestParam(value = "userName", required = true) String userName) {
+        return userService.findByName(userName);
     }
 
 }
