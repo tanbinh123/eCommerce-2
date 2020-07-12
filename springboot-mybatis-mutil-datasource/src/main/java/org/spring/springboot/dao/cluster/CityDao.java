75a96c692f845d43c9773c8018b996a812ed00d3
branwn <50729260+branwn@users.noreply.github.com>
2020-07-12 14:39:23 +0800

    add cityRestApi

--- springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/dao/cluster/CityDao.java
+++ springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/dao/cluster/CityDao.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.spring.springboot.domain.City;
 
+import java.util.List;
+
 /**
  * 城市 DAO 接口类
  *
@@ -18,5 +20,27 @@ public interface CityDao {
      * @param cityName 城市名
      */
     City findByName(@Param("cityName") String cityName);
-    City findById(@Param("cityId") long cityId);
+
+
+    /**
+     * 获取城市信息列表
+     *
+     * @return
+     */
+    List<City> findAllCity();
+
+
+    /**
+     * 根据城市 ID，获取城市信息
+     *
+     * @param id
+     * @return
+     */
+    City findById(@Param("id") Long id);
+
+    Long saveCity(City city);
+
+    Long updateCity(City city);
+
+    Long deleteCity(Long id);
 }
