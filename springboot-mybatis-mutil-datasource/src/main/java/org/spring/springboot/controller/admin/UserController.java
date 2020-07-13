8fc7522cbe595b9ca8706b39f66300de000c77ae
branwn <50729260+branwn@users.noreply.github.com>
2020-07-13 16:37:01 +0800

    完成搜索

--- springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/controller/admin/UserController.java
+++ springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/controller/admin/UserController.java
@@ -1,15 +1,15 @@
 package org.spring.springboot.controller.admin;
 
+import org.spring.springboot.domain.Greeting;
 import org.spring.springboot.domain.User;
 import org.spring.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,17 +23,43 @@ public class UserController {
 
 
 
-    @RequestMapping("/userList")
-    public String userList2(ModelMap map) {
-        map.addAttribute("host", "User list");
+//    @RequestMapping("/userList")
+//    public String userList2(ModelMap map) {
+//        map.addAttribute("host", "User list");
+//        List<User> List = userService.listAll();
+//        map.addAttribute("List", List);
+//        return "admin/userList";
+//    }
+
+    @GetMapping("/userList")
+    public String listAllUsers(Model model) {
+        model.addAttribute("host", "User list");
         List<User> List = userService.listAll();
-        map.addAttribute("List", List);
+        model.addAttribute("List", List);
+        model.addAttribute("user", new User());
         return "admin/userList";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public User findByName(@RequestParam(value = "userName", required = true) String userName) {
-        return userService.findByName(userName);
+    @PostMapping("/userList")
+    public String findByName(Model model, @ModelAttribute User user) {
+        String myUserName = user.getUserName();
+        List<User> List;
+        List = new ArrayList<User>();
+        List.add(userService.findByName(myUserName));
+        model.addAttribute("List", List);
+        return "admin/userList";
     }
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
+
 
 }
